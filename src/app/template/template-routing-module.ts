02edb63d7868443ddb0a6d9a80a6da8ec6919dca
DiorgenesSouza <diorgeneslimadesouza833@gmail.com>
2026-01-09 17:04:41 -0300

    Criação do Componente Categoria

--- src/app/template/template-routing-module.ts
+++ src/app/template/template-routing-module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 
-
 const routes: Routes = [
   {
     path: '',
-    component: Layout
+    component: Layout,
+    children: [
+      {
+        path: 'categorias',
+        loadChildren: () =>
+          import('../categorias/categorias-module').then(m => m.CategoriasModule)
+      }
+    ]
   }
 ];
 
