6682fa148549ac90f07954cd50d353e474ba8201
DiorgenesSouza <diorgeneslimadesouza833@gmail.com>
2026-01-13 17:08:55 -0300

    implementação da Galeria

---
+++ src/app/galeria/galeria/galeria.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria-model';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
+
+@Component({
+  selector: 'app-galeria',
+  standalone: false,
+  templateUrl: './galeria.html',
+  styleUrl: './galeria.scss',
+})
+export class Galeria implements OnInit {
+
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
+
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ){}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas()
+    .subscribe(categorias => this.categoriasFiltro = categorias)
+
+    this.lugarService.obterTodos()
+    .subscribe(lugaresResposta => this.lugares = lugaresResposta);
+    
+  }
+
+}
